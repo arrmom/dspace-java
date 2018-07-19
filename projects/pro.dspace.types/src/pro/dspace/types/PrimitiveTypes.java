1ef660522a05a979418fd366308d6fe8a6ad7c51
Ramil R. Akhmerov <arr@quickpay.ru>
2018-07-19 18:38:32 +0700

    types: PrimitiveTypes - сделали работу с char

--- projects/pro.dspace.types/src/pro/dspace/types/PrimitiveTypes.java
+++ projects/pro.dspace.types/src/pro/dspace/types/PrimitiveTypes.java
@@ -1,5 +1,7 @@
 package pro.dspace.types;
 
+import java.nio.charset.Charset;
+
 /**
  * 
  * @author mom
@@ -19,6 +21,8 @@ public class PrimitiveTypes {
 		realNumbers();
 		System.out.println("--- boolean ------");
 		testBoolean();
+		System.out.println("--- char ------");
+		testChar();
 	}
 
 	private void intNumbers() {
@@ -26,6 +30,10 @@ public class PrimitiveTypes {
 		short shortVar = (short) (month * 100);
 		int index = -234234;
 		long time = 12312342343L;
+		
+		long code = 0x123ff;
+		
+		int code2 = 0b111000;
 
 		System.out.println("test = " + test);
 		System.out.println("month = " + month);
@@ -41,6 +49,10 @@ public class PrimitiveTypes {
 		
 		System.out.println("3 % 2 = " + (3 % 2));
 		System.out.println("3 / 2 = " + (3 / 2));
+		
+		System.out.println("code = " + code + ", hex = " + Long.toHexString(code));
+		
+		System.out.println("code2 = " + code2 + ", bin = " + Integer.toBinaryString(code2));
 	}
 	
 	private void realNumbers() {
@@ -55,7 +67,7 @@ public class PrimitiveTypes {
 
 	private void testBoolean() {
 		boolean b = true;
-		boolean a = 2 < 3;
+		boolean a = 2 < 3;		
 		System.out.println("b = " + b);
 		System.out.println("a = " + a);
 		System.out.println("4 > 6 = " + (4 > 6));
@@ -65,7 +77,31 @@ public class PrimitiveTypes {
 	}
 
 	private void testChar() {
-		// TODO
+		char a = 'a';
+		char b = 'П';
+		char c = '+';
+		
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		
+		System.out.println("---------");
+		for(char ch = 32; ch < 1100; ++ch) {
+			System.out.print("" + ((int) ch) + " " + ch + "  ");
+			if ((ch % 20) == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+		
+		String s = "Hello!";
+		for(int i = 0; i < s.length(); ++i) {
+			System.out.println("pos " + i + ", " + s.charAt(i));
+		}
+		
+		s += b;
+		System.out.println("s = " + s);
+		System.out.println("current charset = " + Charset.defaultCharset());
 	}
 	
 }
