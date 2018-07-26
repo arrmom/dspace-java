cf2f3acde43819f16cfc8d3453e885b4794ed6e9
Ramil R. Akhmerov <arr@quickpay.ru>
2018-07-26 18:18:02 +0700

    operations: CompareOperations - рассмотренаы булевы операции & | ^ && ||

--- projects/pro.dspace.operations/src/pro/dspace/operations/CompareOperations.java
+++ projects/pro.dspace.operations/src/pro/dspace/operations/CompareOperations.java
@@ -6,7 +6,7 @@ package pro.dspace.operations;
  *
  */
 public class CompareOperations {
-	
+
 	public void execute() {
 		int a = 3;
 		int b = 4;
@@ -17,6 +17,32 @@ public class CompareOperations {
 		System.out.println("a <= b = " + (a <= b));
 		System.out.println("a >= b = " + (a >= b));
 		System.out.println("a == b = " + (a == b));
+		System.out.println("a != b = " + (a != b));
+		// & | ! && || ^
+		System.out.println("false & false = " + (false & false)); // AND
+		System.out.println("true & false = " + (true & false));
+		System.out.println("false & true = " + (false & true));
+		System.out.println("true & true = " + (true & true));
+		System.out.println("false | false = " + (false | false)); // OR
+		System.out.println("true | false = " + (true | false));
+		System.out.println("false | true = " + (false | true));
+		System.out.println("true | true = " + (true | true));
+		System.out.println("false ^ false = " + (false ^ false)); // XOR - исключающее или
+		System.out.println("true ^ false = " + (true ^ false));
+		System.out.println("false ^ true = " + (false ^ true));
+		System.out.println("true ^ true = " + (true ^ true));
+		System.out.println("!false = " + (!false)); // NOT
+		System.out.println("!true = " + (!true));
+		System.out.println("!(a < b) = " + (!(a < b)));
+
+		int x = 3;
+		int y = 1;
+		boolean res = x < 2 && y++ < 10;
+		System.out.println("(&&) res = " + res);
+		System.out.println("y = " + y);
+		res = x < 2 & y++ < 10;
+		System.out.println("(&&) res 2 = " + res);
+		System.out.println("y = " + y);
 	}
 
 }
