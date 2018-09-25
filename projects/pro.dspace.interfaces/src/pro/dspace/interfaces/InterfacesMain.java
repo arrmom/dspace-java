a786a92db641e4768c86eab4a584b08ede7dc7af
Ramil R. Akhmerov <arr@quickpay.ru>
2018-09-25 18:51:21 +0700

    заложили проект pro.dspace.interfaces для темы "Интерфейсы"

---
+++ projects/pro.dspace.interfaces/src/pro/dspace/interfaces/InterfacesMain.java
@@ -0,0 +1,16 @@
+package pro.dspace.interfaces;
+
+/**
+ * Интерфейсы.
+ * 
+ * @author mom
+ *
+ */
+public class InterfacesMain {
+
+	public static void main(String[] args) {
+		System.out.println("Интерфейсы");
+		// TODO
+	}
+
+}
