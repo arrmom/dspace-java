79409c9e6f454a455acad001fbe5f2477ddafad6
Ramil R. Akhmerov <arr@quickpay.ru>
2018-09-27 19:01:59 +0700

    заложили проект pro.dspace.exceptions

---
+++ projects/pro.dspace.exceptions/src/pro/dspace/exceptions/ExceptionsMain.java
@@ -0,0 +1,15 @@
+package pro.dspace.exceptions;
+
+/**
+ * Исключения.
+ * 
+ * @author mom
+ *
+ */
+public class ExceptionsMain {
+
+	public static void main(String[] args) {
+		System.out.println("Exceptions");
+	}
+
+}
