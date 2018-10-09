b7f67756f60569f8f3fbc4b878d09ac501af03e1
Ramil R. Akhmerov <arr@quickpay.ru>
2018-10-09 18:22:01 +0700

    заложили проект pro.dspace.enums

---
+++ projects/pro.dspace.enums/src/pro/dspace/enums/EnumsMain.java
@@ -0,0 +1,15 @@
+package pro.dspace.enums;
+
+/**
+ * Перечисления.
+ * 
+ * @author mom
+ *
+ */
+public class EnumsMain {
+
+	public static void main(String[] args) {
+		System.out.println("---- Перечисления ----");
+	}
+
+}
