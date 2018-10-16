925cba73d44ddd4e30141fdb0ff3d2fa27f1f2d4
FilinPW <mail@filin.pw>
2018-10-17 01:01:12 +0700

    Создан проект pro.dspace.home.ivan.bag2.

---
+++ projects/part2/pro.dspace.home.ivan.bag/pro.dspace.home.ivan.bag2/src/main/java/pro/dspace/home/ivan/bag2/IvanBag.java
@@ -0,0 +1,61 @@
+package pro.dspace.home.ivan.bag2;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Множество целых чисел с дубликатами.
+ * 
+ * @author Filin.PW
+ *
+ */
+public class IvanBag implements Bag<Object> {
+
+	/** Хранилище элементов множества и информации о их количестве в этом множестве. */
+	private final Map<Object, Integer> bag = new HashMap<Object, Integer>();
+
+	@Override
+	public synchronized void add(Object e) {
+		if (bag.containsKey(e)) {
+			int val = bag.get(e);
+			bag.put(e, ++val);
+		} else {
+			bag.put(e, 1);
+		}
+	}
+
+	@Override
+	public synchronized boolean remove(Object e) {
+		if (bag.containsKey(e)) {
+			if (bag.get(e) > 1) {
+				int val = bag.get(e);
+				bag.put(e, --val);
+			} else {
+				bag.remove(e);
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public synchronized int size() {
+		int size = 0;
+		for (int val : bag.values()) {
+			size += val;
+		}
+		return size;
+	}
+
+	@Override
+	public synchronized int count(Object e) {
+		if (bag.containsKey(e)) {
+			return bag.get(e);
+		} else {
+			return 0;
+		}
+	}
+
+}
